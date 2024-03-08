8408133d461f6b7b34b2e310ecb21bd1fbd8a349
yaxley-peaks <epiclycoolgaemer@gmail.com>
2024-03-08 23:55:20 +0530

    Second

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+const mathjaxPlugin = require("eleventy-plugin-mathjax");
+
+module.exports = function (eleventyConfig) {
+  // Return your Object options:
+  eleventyConfig.addPassthroughCopy("*.jpg");
+  eleventyConfig.addPassthroughCopy("*.png");
+    eleventyConfig.addPassthroughCopy("*.gif");
+  eleventyConfig.addPlugin(mathjaxPlugin);
+  return {
+    passthroughFileCopy: true
+  };
+};
